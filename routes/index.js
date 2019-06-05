c40ec70b6b32f83339140e5e19951899e36b5cae
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2019-06-05 15:11:43 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,24 @@ cloudinary.config({
 const fs = require('fs-extra')
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("hello world")
+router.get("/getAllImages", (req, res) => {
+    Photo.find(function (err, Photo) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(Photo);
+        }
+    })
 })
 
 router.post("/images/add", async (req, res) => {
     const result = await cloudinary.v2.uploader.upload(req.file.path);
     const newPhoto = new Photo({
         imageURL: result.secure_url,
-        public_id: result.public_id
+        public_id: result.public_id,
+        title: req.body.title,
+        description: req.body.description
+
     });
     await newPhoto.save()
         .then(newPhoto => {
@@ -30,6 +39,18 @@ router.post("/images/add", async (req, res) => {
             res.status(400).json.send('adding new newPhoto failed')
         })
     await fs.unlink(req.file.path);
+})
+
+router.delete("/image/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    const photo = await Photo.findByIdAndDelete(id)
+    await cloudinary.v2.uploader.destroy(photo.public_id)
+        .then(photo => {
+            res.json('photo deleted');
+        })
+        .catch(err => {
+            res.status(400).send("delete no possible")
+        })
 })
 
 
